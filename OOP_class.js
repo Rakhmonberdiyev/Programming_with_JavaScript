5f233afb7a574b8b02222b651766b58d1a6d6801
RAximberdi <r.raxonberdiyev@newuu.uz>
2024-02-21 23:55:48 +0500

    4

--- OOP_class.js
+++ OOP_class.js
@@ -1,27 +1,60 @@
-class StationaryBike {
-    constructor(position, gears) {
-        this.position = position
-        this.gears = gears
+class Train {
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    }
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+    getSelf() {
+        console.log(this);
+    }
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
     }
 }
 
-class Treadmill {
-    constructor(position, modes) {
-        this.position = position
-        this.modes = modes
+class HighSpeedTrain extends Train {
+    constructor(passengers, highSpeedOn, color, lightsOn) {
+        super(color, lightsOn);
+        this.passengers = passengers;
+        this.highSpeedOn = highSpeedOn;
+    }
+    toggleHighSpeed() {
+        this.highSpeedOn = !this.highSpeedOn;
+        console.log('High speed status:', this.highSpeedOn);
+    }
+    toggleLights() {
+        super.toggleLights();
+        super.lightsStatus();
+        console.log('Lights are 100% operational.');
     }
 }
 
-class Gym {
-    constructor(openHrs, stationaryBikePos, treadmillPos) {
-        this.openHrs = openHrs
-        this.stationaryBike = new StationaryBike(stationaryBikePos, 8)
-        this.treadmill = new Treadmill(treadmillPos, 5)
-    }
-}
+var myFirstTrain = new Train('red', false);
+console.log(myFirstTrain); // Train {color: 'red', lightsOn: false}
+var mySecondTrain = new Train('blue', false);
+var myThirdTrain = new Train('blue', false);
 
-var boxingGym = new Gym("7-22", "right corner", "left corner")
+var train4 = new Train('red', false);
+train4.toggleLights(); // undefined
+train4.lightsStatus(); // Lights on? true
+train4.getSelf(); // Train {color: 'red', lightsOn: true}
+train4.getPrototype(); // {constructor: f, toggleLights: f, ligthsStatus: f, getSelf: f, getPrototype: f}
 
-console.log(boxingGym.openHrs) //
-console.log(boxingGym.stationaryBike) //
-console.log(boxingGym.treadmill) //
+var train5 = new Train('blue', false);
+var highSpeed1 = new HighSpeedTrain(200, false, 'green', false);
+
+train5.toggleLights(); // undefined
+train5.lightsStatus(); // Lights on? true
+highSpeed1.toggleLights(); // Lights on? true, Lights are 100% operational.
+highSpeed1.toggleHighSpeed(); // High speed status: true
+highSpeed1.toggleHighSpeed(); // High speed status: false
+highSpeed1.getSelf(); // HighSpeedTrain {color: 'green', lightsOn: true, passengers: 200, highSpeedOn: false}
+highSpeed1.getPrototype(); // {constructor: f, toggleHighSpeed: f}
+train5.getPrototype(); // {constructor: f, toggleLights: f, ligthsStatus: f, getSelf: f, getPrototype: f}
+train5.color = 'yellow';
