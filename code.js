f8b74050012704db7e1fa8e57e9c8b10f2fc47a9
RAximberdi <r.raxonberdiyev@newuu.uz>
2024-02-24 00:22:12 +0500

    9

--- code.js
+++ code.js
@@ -1,20 +1,4 @@
-const fruits = ['kiwi','mango','apple','pear'];
-function appendIndex(fruit, index) {
-    console.log(`${index}. ${fruit}`)
-}
-    fruits.forEach(appendIndex);
-const veggies = ['onion', 'garlic', 'potato'];
-veggies.forEach( function(veggie, index) {
-    console.log(`${index}. ${veggie}`);
-});
-const nums = [0,10,20,30,40,50];
-function app(num) {
-    return num > 20;
-}
-nums.filter(app);
-nums.filter( function(num) {
-    return num > 10;
-})
-    nums.map( function(num) {
-    return num / 10;
-})
+var message = "Hello";
+message += " World!";
+message = "Goodbye!";
+console.log(message);
